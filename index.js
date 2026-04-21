524f2fbc462f7f51a8f5a5d458575ac110ff8d85
Rahul Jaggi <rahuljaggi@Rahuls-MacBook-Pro.local>
2026-04-21 11:26:35 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const express = require("express");
+const { Pool } = require("pg");
+
+const app = express();
+app.use(express.json());
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+app.get("/", (req, res) => {
+  res.send("Server running ✅");
+});
+
+// sample API
+app.get("/users", async (req, res) => {
+  const result = await pool.query("SELECT * FROM users");
+  res.json(result.rows);
+});
+
+app.listen(3000, () => console.log("Server started"));
